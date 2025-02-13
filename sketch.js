9e292afcd1a14ec7ce3a690abfcda04261582ee8
Elias Jackson <ejthedj0@gmail.com>
2025-02-13 14:49:04 -0800

    Replace with correct file

--- sketch.js
+++ sketch.js
@@ -1,83 +1,108 @@
-let car, environment;
-let offsetX = 0;
-let offsetY = 30;
-let rotateAmount = 0;
+let textureData;
+let phaseSlider;
+
 function setup() {
-  createCanvas(300, 300);
-  car = new Car();
-  environment = new Environment();
-}
+  createCanvas(400, 400);
+  textureData = Array(40)
+    .fill(0)
+    .map(() => ({
+      size: random(25),
+      x: random(width),
+      y: random(height),
+      shade: random(50) + 200,
+    }));
 
+  phaseSlider = createSlider(1, 28, 1, 1);
+  phaseSlider.position(30, 10);
+  phaseSlider.size(340);
+}
 function draw() {
-  background(255);
-
-  // rotate around center
-  rotateAmount += 0.001;
-  translate(width/2, height/2);
-  rotate(rotateAmount);
-  translate(-width/2, -height/2);
-
-  environment.update();
-  environment.draw();
-  car.draw();
-
+  background(0);
+  stroke(255);
+  text(`${phaseSlider.value()}`, 10, 26);
+  drawMoon(300, color(130, 15, 130), phaseSlider.value());
 }
 
-class Car {
+function drawMoon(size, col, phase) {
+  // shape outline (for debugging)
+  // strokeWeight(1);
+  // stroke(255);
+  // noFill();
+  // outer(phase)();
+  // inner(phase)();
 
-  draw() {
+  stroke(0);
+  push();
 
-    line(80, 60, 220, 60);
-    line(220, 60, 260, 100);
-    line(260, 100, 260, 120);
-    line(260, 120, 220, 120);
-    line(180, 120, 130, 120);
-    line(90,  120, 40,  120);
-    line(40, 100, 40, 120);
-    line(40, 100, 80, 60);
+  // mask
+  clip(outer(phase));
+  clip(inner(phase));
 
-    circle(200, 120, 40);
-    circle(110, 120, 40);
-  }
+  // texture
+  background(lerpColor(color(220), col, 0.1));
+  strokeWeight(0);
+  textureData.forEach(({ x, y, size, shade }) => {
+    fill(lerpColor(color(shade, shade, shade), col, 0.1));
+    circle(x, y, size);
+  });
+
+  pop();
+
+  // shade
+  coverHalf(phase);
+  innerArc(phase);
 }
 
-class Environment {
-  constructor() {
-    this.lines = [];
-    this.spawnLikeliness = 0.05;
-  }
+function outer(phase) {
+  return () => {
+    arc(200, 200, 300, 300, 0, 2 * PI);
+  };
+}
 
-  update() {
-    this.spawnLikeliness += 0.001;
-    if (Math.random() < this.spawnLikeliness) {
-      this.lines.push(new Line(this.spawnLikeliness * 200));
-      // print('new line')
-    }
-    for (const line of this.lines) {
-      line.update();
-    }
-  }
-
-  draw() {
-    for (const line of this.lines) {
-      line.draw();
-    }
+function coverHalf(phase) {
+  fill(0);
+  if (phase < 8) {
+    rect(50, 50, 150, 350);
+  } else if (phase >= 22) {
+    rect(200, 50, 350, 350);
   }
 }
 
-class Line {
-  constructor(speed) {
-    this.x = width;
-    this.y = random(height);
-    this.speed = speed;
-    this.length = random(30);
+function innerArc(phase) {
+  fill(0);
+  // stroke(255);
+  if (phase < 8) {
+    const control = -800 + phase * 130;
+    curve(control, 50, 200, 50, 200, 350, control, 350);
+  } else if (phase > 22) {
+    const control = -800 + (phase % 15) * 160;
+    curve(control, 50, 200, 50, 200, 350, control, 350);
   }
+}
 
-  update() {
-    this.x -= this.speed;
-  }
+function inner(phase) {
+  return () => {
+    if (phase >= 8 && phase < 15) {
+      const control = -800 + phase * 130;
+      curve(control, 50, 200, 50, 200, 350, control, 350);
+      rect(200, 50, 350, 350);
+    } else if (phase >= 16 && phase < 22) {
+      const control = -800 + (phase % 15) * 130;
+      curve(control, 50, 200, 50, 200, 350, control, 350);
+      rect(50, 50, 150, 350);
+    } else {
+      // deactivate clipping
+      rect(0, 0, width, height);
+    }
+  };
+}
 
-  draw() {
-    line(this.x, this.y, this.x + this.length, this.y);
-  }
+function textureCircle(tintColor) {
+  const size = random(25);
+  const xPos = random(width);
+  const yPos = random(height);
+  const shade = random(50) + 200;
+  fill(lerpColor(color(shade, shade, shade), tintColor, 0.1));
+  strokeWeight(0);
+  circle(xPos, yPos, size);
 }
